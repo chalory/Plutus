e0583f35dfc8b8babacad6434ed535986a21cdc1
chalory <chantalrain.pino@gmail.com>
2022-12-03 18:54:36 +0800

    belh

--- finhacks/projectApp/views.py
+++ finhacks/projectApp/views.py
@@ -6,3 +6,8 @@ from django.template import loader
 def index(request):
   template = loader.get_template('first.html')
   return HttpResponse(template.render())
+
+
+def quiz(request):
+  template = loader.get_template('quiz.html')
+  return HttpResponse(template.render())
