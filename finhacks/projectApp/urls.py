ebdef342a81db48abb44fec88ad714d68e699913
rvitality <vital.renzjohn@gmail.com>
2022-12-03 18:53:13 +0800

    Updates

--- finhacks/projectApp/urls.py
+++ finhacks/projectApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("needs_and_wants", views.needs_and_wants, name="needs_and_wants"),
+    path("slider", views.slider, name="slider"),
 ]
--- finhacks/projectApp/views.py
+++ finhacks/projectApp/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def index(request):
-  template = loader.get_template('first.html')
-  return HttpResponse(template.render())
+def index(response):
+    template = loader.get_template("landing.html")
+    return HttpResponse(template.render())
+
+
+def needs_and_wants(response):
+    template = loader.get_template("needs_and_wants.html")
+    return HttpResponse(template.render())
+
+
+def slider(response):
+    template = loader.get_template("slider.html")
+    return HttpResponse(template.render())
